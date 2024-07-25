cc48dc69ad18bfe28a197402a4aad5e109945f00
deirdre <dleeee333@gmail.com>
2024-07-25 17:05:57 +0100

    Course part 2: vue instance

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 let app = new Vue({
     // options object
-    el: "#app"
+    el: "#app",
+    data: {
+        title: 'Deirdre\'s Portfolio',
+        titleHTML: "Deirdre\'s <span class='badge'>Portfolio</span>",
+        projects: [
+            {title: "portfolio", desc: "description"},
+            {title: "Twitter clone", desc: "clone description"}
+        ]
+    }
 })
